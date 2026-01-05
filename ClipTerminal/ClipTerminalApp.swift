ac8aea076fc900cefe8bba031003eb2dd31e83b5
amania-Jailbreak <amania.apple@icloud.com>
2026-01-05 12:46:12 +0900

    Initial Commit

---
+++ ClipTerminal/ClipTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipTerminalApp.swift
+//  ClipTerminal
+//
+//  Created by amania on 2026/01/05.
+//
+
+import SwiftUI
+
+@main
+struct ClipTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
